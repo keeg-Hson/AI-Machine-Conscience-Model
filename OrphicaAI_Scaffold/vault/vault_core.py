f67c413503125bba55a852024781892a5ed43621
keeg-Hson <kjalexanderbusiness@gmail.com>
2025-04-14 17:01:12 -0300

    vault_core.py pseudo code roughly laid out

--- OrphicaAI_Scaffold/vault/vault_core.py
+++ OrphicaAI_Scaffold/vault/vault_core.py
@@ -1,1 +1,33 @@
-# Vault Core
+# Vault Core 
+##Core Entry Structure
+def create_entry():
+"""
+Creates and returns fully formulated memory entry from user input.
+"""
+
+def get_entry(entry_id:str):
+    """
+    retrieves user entry by ID
+    """
+
+def recall_by_symbol():
+    """
+    retrieves user entries with use of symbolic tagging
+    """
+
+def resurface_candidate(content_vector:dict) -> str:
+    """
+    returns a list of letent memories that are likely to become relevant to user down the road.
+    -would compare embeddings/emotinal vectors within full release
+    """
+
+##Memory Maintenance:
+def reinforce(entry_id:str, boost: float=0.01):
+    """
+    fortifies salience of a given user entry, as if it were being consciously recalled.
+    """
+
+def decay_all():
+    """
+    global dacay tick: lowers the weighting factor associated with all user entires over time.
+    """
