a43d9f8ec3072722c440e7bf97fd391f33ef997d
keeg-Hson <kjalexanderbusiness@gmail.com>
2025-04-14 17:13:09 -0300

    built in a temporary memory store function for prototyping purposes. as well, declared necessary libraries for this layer of the project

--- OrphicaAI_Scaffold/vault/vault_core.py
+++ OrphicaAI_Scaffold/vault/vault_core.py
@@ -1,4 +1,16 @@
-# Vault Core 
+# Vault Core
+from datetime import datetime  #for timestamping new memories
+from uuid import uuid4 #generation of unique IDs for each memory entry in question
+from .emotion_vectorizer import classify_emotions #Analyzes the emotional tone associated with eahc piece of contetn
+from .symbolic_index import tag_entry, find_symbolic_associations #symbolic tagging/index linking fucntionality
+from .decay import decay_weights, reinforce_entry #weighting and decay of memory entries
+
+#TEMPORARY: in-built memory store for prototyping purposes
+VAULT={} #MAIN MEMORY VAULT (OR "SUBCONSCIOUS" MEMORY BANK EFFECGTIVELY)
+SYMBOLIC_INDEX={} #SYMBOL TO ENTRY LOOKIP TABLE (SIMILAR TO ARCHETYOPAL ANCHORS OR THEMATIC LINKS)
+
+
+
 ##Core Entry Structure
 def create_entry():
 """
